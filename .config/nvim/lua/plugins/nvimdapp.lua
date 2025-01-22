6ec537224c4d9d4dd9cb217a36000e85554f4280
marvindore <hi@marvindore.com>
2025-01-22 08:59:22 -0500

    Remove dapui

--- .config/nvim/lua/plugins/nvimdapp.lua
+++ .config/nvim/lua/plugins/nvimdapp.lua
@@ -17,7 +17,7 @@ return {
 		"theHamsta/nvim-dap-virtual-text",
 		"nvim-telescope/telescope-dap.nvim",
 		"jbyuki/one-small-step-for-vimkind",
-		"rcarriga/nvim-dap-ui",
+    { "igorlfs/nvim-dap-view", opts = {} },
 		{
 			"microsoft/vscode-js-debug",
 			build = isWindows and "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && Move-Item -Path dist -Destination out" or "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
@@ -370,80 +370,6 @@ return {
 
 		require("telescope").load_extension("dap")
 		require("nvim-dap-virtual-text").setup({})
-		require("dapui").setup({
-			-- icons = { expanded = '?', collapsed = '?' },
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-			force_buffers = true,
-			-- Use this to override mappings for specific elements
-			element_mappings = {
-				-- Example:
-				-- stacks = {
-				--   open = "<CR>",
-				--   expand = "o",
-				-- }
-			},
-			mappings = {
-				-- Use a table to apply multiple mappings
-				expand = { "<CR>", "<2-LeftMouse>" },
-				open = "o",
-				remove = "d",
-				edit = "e",
-				repl = "r",
-				toggle = "t",
-			},
-			-- Expand lines larger than the window
-			-- Requires >= 0.7
-			expand_lines = vim.fn.has("nvim-0.7") == 1,
-			layouts = {
-				{
-					elements = {
-						-- Elements can be strings or table with id and size keys.
-						{ id = "scopes", size = 0.25 },
-						"breakpoints",
-						"stacks",
-						"watches",
-					},
-					size = 40, -- 40 columns
-					position = "left",
-				},
-				{
-					elements = {
-						"repl",
-						"console",
-					},
-					size = 0.25, -- 25% of total lines
-					position = "bottom",
-				},
-			},
-			controls = {
-				enabled = true,
-				-- Display controls in this element
-				element = "repl",
-				icons = {
-					pause = "",
-					play = "",
-					step_into = "",
-					step_over = "",
-					step_out = "",
-					step_back = "",
-					run_last = "↻",
-					terminate = "□",
-				},
-			},
-			floating = {
-				max_height = nil, -- These can be integers or a float between 0 and 1.
-				max_width = nil, -- Floats will be treated as percentage of your screen.
-				border = "single",
-				mappings = {
-					close = { "q", "<Esc>" },
-				},
-			},
-			windows = { indent = 1 },
-			render = {
-				max_type_length = nil, -- Can be integer or nil.
-				max_value_lines = 100, -- Can be integer or nil.
-			},
-		})
 
 		-- nvim-dap-virtual-text. Show virtual text for current frame
 		vim.g.dap_virtual_text = true
