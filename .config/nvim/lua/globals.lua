350965df89a1afba4fc52e72db35f454efa5592f
Marvin Dore Jr <marvindore@gmail.com>
2025-05-15 13:30:00 -0400

    Update globals.lua
    Fix type-o for global vim.g.isWindowsOs

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -8,5 +8,5 @@ vim.g.enablePython = false
 vim.g.enableAvante = false
 vim.g.enableCopilot = false
 vim.g.isWindowsOs = vim.fn.has('win32') == 1
-vim.g.homeDir = vim.g.isWindows and os.getenv("USERPROFILE") or os.getenv('HOME')
-vim.g.neovim_home = vim.g.isWindows and vim.g.homeDir .. '/AppData/Local/nvim-data' or vim.g.homeDir .. '/.local/share/nvim'
+vim.g.homeDir = vim.g.isWindowsOs and os.getenv("USERPROFILE") or os.getenv('HOME')
+vim.g.neovim_home = vim.g.isWindowsOs and vim.g.homeDir .. '/AppData/Local/nvim-data' or vim.g.homeDir .. '/.local/share/nvim'
