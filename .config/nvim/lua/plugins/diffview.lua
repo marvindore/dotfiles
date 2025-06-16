856cdec0c4d80c7c9be4e94e49b64520ed3fde04
marvindore <hi@marvindore.com>
2025-06-16 15:57:04 -0400

    Change diffview to 4way merge

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -40,7 +40,7 @@ return {
         },
         merge_tool = {
           -- Config for conflicted files in diff views during a merge or rebase.
-          layout = "diff3_mixed",
+          layout = "diff4_mixed",
           disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
           winbar_info = true,         -- See ':h diffview-config-view.x.winbar_info'
         },
