61dfc6e9f158cc11bbb9c676d5b741b020c38173
Marvin Dore <hi@marvindore.com>
2025-12-26 11:49:22 -0500

    Update angular and snacks

--- dot_config/nvim/lsp/angularls.lua
+++ dot_config/nvim/lsp/angularls.lua
@@ -1,4 +1,13 @@
+local project_modules = vim.fn.getcwd() .. '/node_modules'
+local cmd = {
+  'ngserver', '--stdio',
+  '--tsProbeLocations', project_modules,
+  '--ngProbeLocations', project_modules,
+  '--angularCoreVersion", "15.2.10'
+}
+
 return {
-  cmd = { "ngserver", "--stdio", "--tsProbeLocations", "", "--ngProbeLocations", "", "--angularCoreVersion", "" },
-  filetypes = { "angular", "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" }
+  cmd = cmd,
+  filetypes = { "angular", "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
+  root_markers = { "angular.json", "nx.json" }
 }
